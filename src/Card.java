d17d8e765a8ac07bb431b3eb00469e143025bb59
devohda <fffvvv6@gmail.com>
2020-11-03 00:30:03 +0900

    코드 정리

--- src/Card.java
+++ src/Card.java
@@ -3,15 +3,10 @@ import java.awt.*;
 import java.util.Random;
 public class Card {
 
-    private CardColor cardColorSet[] = CardColor.values();
-    private Shape shapeSet[] = Shape.values();
-    private BackgroundColor backgroundColorSet[] = BackgroundColor.values();
+    private CardColor cardColor;
+    private Shape shape;
+    private BackgroundColor backgroundColor;
 
-    public CardColor cardColor;
-    public Shape shape;
-    public BackgroundColor backgroundColor;
-
-    Random random = new Random();
 
     //constructor
     public Card(){
@@ -27,32 +22,86 @@ public class Card {
     }
 
     //set 함수
-    private void setCardColor(){
-        int index = random.nextInt(3);
-        cardColor = cardColorSet[index];
+    //랜덤으로 배경색, 모양, 도형의 색 결정
+    Random random = new Random();
+    
+    public void setCardColor(){ //
+        CardColor cardColorSet[] = CardColor.values();
+        cardColor = cardColorSet[random.nextInt(3)];
     }
-    private void setShape(){
-        int index = random.nextInt(3);
-        shape = shapeSet[index];
+    public void setShape(){
+        Shape shapeSet[] = Shape.values();
+        shape = shapeSet[random.nextInt(3)];
     }
-    private void setBackgroundColor(){
-        int index = random.nextInt(3);
-        backgroundColor = backgroundColorSet[index];
+    public void setBackgroundColor(){
+        BackgroundColor backgroundColorSet[] = BackgroundColor.values();
+        backgroundColor = backgroundColorSet[random.nextInt(3)];
     }
     
     //get 함수
     public CardColor getCardColor(){
         return cardColor;
     }
-
     public Shape getShape(){
         return shape;
     }
-
     public BackgroundColor getBackgroundColor() {
         return backgroundColor;
     }
 
 
+    //카드 그리기(GUI)
+    class Figure extends JPanel{
 
+        public void paintComponent(Graphics paint){
+
+            super.paintComponent(paint); //패널 내 잔상 지우기
+            
+            //카드 배경 칠하기
+            switch (backgroundColor){
+                case WHITE -> {
+                    super.setBackground(Color.WHITE);
+                }
+                case GRAY -> {
+                    super.setBackground(Color.GRAY);
+                }
+                case BLACK -> {
+                    super.setBackground(Color.BLACK);
+                }
+            }
+            
+            // 도형 색깔 결정하기
+            switch (cardColor){
+                case RED -> {
+                    paint.setColor(Color.RED);
+                }
+                case YELLOW -> {
+                    paint.setColor(Color.YELLOW);
+                }
+                case GREEN -> {
+                    paint.setColor(Color.GREEN);
+                }
+            }
+            
+            //도형 그리기
+            switch (shape){
+
+                case CIRCLE -> { //원 그리기
+                    paint.fillOval(0,0,100,100);
+                }
+                case SQUARE -> { // 사각형 그리기
+                    paint.fillRect(0,0,100,100);
+                }
+                case TRIANGLE -> { //삼각형 그리기
+                    int x[] = { 0, 50, 100 };
+                    int y[] = { 100, 0, 100 };
+                    paint.fillPolygon( x, y, 3 );
+                }
+            }
+        }
+    }
+
+    public Figure drawCard(){
+        return new Figure();
+    }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 
 public class Game extends JPanel{
@@ -20,8 +19,9 @@ public class Game extends JPanel{
         background.setBackground(new Color(230,230,230));
 
         JPanel board = makeBoard();
-        background.add(board);
+        calculateHap();
 
+        background.add(board);
         frame.getContentPane().add(background); //프레임의 컨텐트팬을 background 패널로 변경
         //frame.pack();
         frame.setVisible(true);
@@ -34,7 +34,6 @@ public class Game extends JPanel{
     /******** 게임 판 만들기 *********/
     /*******************************/
 
-
     private Card[][] boardInfo;
     public void setGameBoard(){
         boardInfo = new Card[3][3];
@@ -45,6 +44,8 @@ public class Game extends JPanel{
             }
         }
     }//setGameBoard()
+
+
 
     /*******************************/
     /******** 게임 판 그리기 *********/
@@ -57,76 +58,26 @@ public class Game extends JPanel{
         gameBoard.setBounds(30,30,900,900);
         gameBoard.setLayout(null);
 
-        setGameBoard();
+        setGameBoard(); //게임판 새로 만들기
+        //게임판 그리기
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                Figure figure = new Figure(boardInfo[i][j].getCardColor(),boardInfo[i][j].getShape(),boardInfo[i][j].getBackgroundColor());
-                figure.setBounds(300*(i) + 50, 300*(j) + 50, 200, 200);
-
-
-                boardInfo[i][j].getCardInfo();
+                Card.Figure figure = boardInfo[i][j].drawCard();
+                figure.setBounds(300*(j) + 50, 300*(i) + 50, 200, 200);
                 gameBoard.add(figure);
             }
         }
         return gameBoard;
     }
 
-    class Figure extends JPanel{
-
-        private CardColor cardColor;
-        private Shape shape;
-        private BackgroundColor backgroundColor;
-
-        public Figure(CardColor c, Shape s, BackgroundColor b){
-            backgroundColor = b;
-            cardColor = c;
-            shape = s;
-        }
-
-        public void paintComponent(Graphics paint){
-
-            super.paintComponent(paint);
-
-            switch (backgroundColor){
-                case WHITE -> {
-                    super.setBackground(Color.WHITE);
-                }
-                case GRAY -> {
-                    super.setBackground(Color.GRAY);
-                }
-                case BLACK -> {
-                    super.setBackground(Color.BLACK);
-                }
-            }
-
-            switch (cardColor){
-                case RED -> {
-                    paint.setColor(Color.RED);
-                }
-                case YELLOW -> {
-                    paint.setColor(Color.YELLOW);
-                }
-                case GREEN -> {
-                    paint.setColor(Color.GREEN);
-                }
-            }
-
-            switch (shape){
-
-                case CIRCLE -> {
-                    paint.fillOval(0,0,100,100);
-                    break;
-                }
-                case SQUARE -> {
-                    paint.fillRect(0,0,100,100);
-                    break;
-                }
-                case TRIANGLE -> {
-                    int x[] = { 0, 50, 100 };
-                    int y[] = { 100, 0, 100 };
-                    paint.fillPolygon( x, y, 3 );       // 삼각형그리기
-                    break;
-                }
+    /*******************************/
+    /******* '합' 집합 구하기 ********/
+    /*******************************/
+    private int hapNum;
+    public void calculateHap(){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                boardInfo[i][j].getCardInfo();
             }
         }
     }
